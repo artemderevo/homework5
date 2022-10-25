0e37fb4972df4ee98cb9c6738c32e84392a40197
q2q2q8 <art-kash@mail.ru>
2022-10-26 02:28:46 +0300

    четвертое задание сделано

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 17;
+        int age = 18;
         if (age >= 18) {
             System.out.println("Поздравляем с совершеннолетием!");
-        }
-        if (age < 18) {
+        } else {
+
             System.out.println("Возраст совершеннолетия не наступил");
         }
     }
