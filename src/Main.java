618f41340994ee08f4f26bc2fb2480fa731bbd4a
q2q2q8 <art-kash@mail.ru>
2022-10-26 02:47:42 +0300

    шестое задание сделано

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        int slots = 60;
-       if (slots >= 0 && slots < 59){
+        int slots = 102;
+       if (slots >= 0 && slots < 30){
             System.out.println("В вагоне есть свободные сидячие места.");
-        }
-       if (slots >= 60 && slots < 102){
-           System.out.println("В вагоне есть стоячие места.");
+        } else if (slots >= 30 && slots < 59) {
+           System.out.println("В вагоне есть половина сидячих мест.");
        }
-       if (slots >= 102){
-           System.out.println("Вагон заполнен");
+        if (slots >= 60 && slots < 81){
+           System.out.println("В вагоне есть стоячие места.");
+       } else if (slots >= 81 && slots <100) {
+            System.out.println("В вагоне половина стоячих мест.");
+        }
+       if (slots <= 101){
+           System.out.println("Вагон почти заполнен.");
+       } else if (slots <= 102){
+           System.out.println("Вагон заполнен.");
        }
 
 
