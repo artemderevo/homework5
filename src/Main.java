352befdbae007f6d369e6e51e1283aadaddc3f53
q2q2q8 <art-kash@mail.ru>
2022-10-26 02:38:55 +0300

    пятое задание сделано

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 16;
+        int age = 25;
         if (age >= 7 && age < 18){
             System.out.println("Ребенок идет в школу");
-        }
-        if (age >= 18 && age < 24){
+        } else if (age >=18 && age < 24) {
             System.out.println("Человек  идет в университет");
         }
-        if (age >= 24){
+        else if (age >= 24){
             System.out.println("Человек идет искать работу");
         }
 
